bd5ca646f0a84a6ab4b7b29cf004ec4e73ce18eb
pandacase <2227119810@qq.com>
2023-11-27 11:01:09 +0800

    Update tcp_sender.cc

--- libsponge/tcp_sender.cc
+++ libsponge/tcp_sender.cc
@@ -28,8 +28,9 @@ using namespace std;
 //! \param[in] fixed_isn the Initial Sequence Number to use, if set (otherwise uses a random ISN)
 TCPSender::TCPSender(const size_t capacity, const uint16_t retx_timeout, const std::optional<WrappingInt32> fixed_isn)
     : _isn(fixed_isn.value_or(WrappingInt32{random_device()()}))
-    , _initial_retransmission_timeout{retx_timeout}
-    , _stream(capacity) {}
+    , _initial_retransmission_timeout(retx_timeout)
+    , _stream(capacity)
+    , _current_retransmission_timeout(retx_timeout) {}
 
 uint64_t TCPSender::bytes_in_flight() const { return {_bytes_in_flight}; }
 
@@ -53,16 +54,38 @@ void TCPSender::fill_window() {
 //! \param window_size The remote receiver's advertised window size
 //! \returns `false` if the ackno appears invalid (acknowledges something the TCPSender hasn't sent yet)
 bool TCPSender::ack_received(const WrappingInt32 ackno, const uint16_t window_size) {
+    uint64_t absolute_ackno = unwrap(ackno, _isn, _acked);
+    if (absolute_ackno >= _next_seqno) {
+        return {false};
+    }
+
     
-    return {};
+
+    return {true};
 }
 
 //! \param[in] ms_since_last_tick the number of milliseconds since the last call to this method
 void TCPSender::tick(const size_t ms_since_last_tick) { 
+    // increase the timer
+    _timer += ms_since_last_tick;
     
+    // check if time out
+    if (_timer > _current_retransmission_timeout) { 
+        // retransmission
+
+
+        if (_receiver_window_size != 0) {
+            // keep track of the number of consecutive retransmissions:
+            _consecutive_retransmission += 1;
+            // Double the RTO:
+            _current_retransmission_timeout *= 2;
+            // start the retransmission timer:
+            _timer = 0;
+        }
+    }
 }
 
-unsigned int TCPSender::consecutive_retransmissions() const { return {}; }
+unsigned int TCPSender::consecutive_retransmissions() const { return {_consecutive_retransmission}; }
 
 void TCPSender::send_empty_segment() {
     
